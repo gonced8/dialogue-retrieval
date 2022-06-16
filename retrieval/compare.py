9f739b248b9a934e1b09d9c6640b7ac1a7a13b96
gonced8 <gonced8@gmail.com>
2022-06-16 11:57:24 -0400

    tables

--- retrieval/compare.py
+++ retrieval/compare.py
@@ -1,23 +1,38 @@
+from difflib import SequenceMatcher
 from itertools import cycle
-import json
+import json, _json
 
-# from bert_score import
+from bert_score import BERTScorer
 from rouge_score.rouge_scorer import RougeScorer
 import streamlit as st
+import textdistance
+from transformers import logging
+
+st.set_page_config(layout="wide")
 
 
 @st.cache(allow_output_mutation=True)
 def init():
     filename_data = "../data/multiwoz/processed/data.json"
-    with open(filename_data, "r") as f:
+    data = read_json(filename_data)
+
+    # Disable warnings of Transformers
+    logging.set_verbosity_error()
+
+    bert_scorer = BERTScorer(lang="en", rescale_with_baseline=True)
+    rouge_scorer = RougeScorer(["rouge1", "rouge2", "rougeL"])
+
+    return data, bert_scorer, rouge_scorer
+
+
+def read_json(filename):
+    with open(filename, "r") as f:
         data = json.load(f)
-
-    scorer = RougeScorer(["rouge1", "rouge2", "rouge3", "rougeL"])
-
-    return data, scorer
+    return data
 
 
 def hide_table_index():
+    # st.table
     hide_table_row_index = """
                 <style>
                 tbody th {display:none}
@@ -26,12 +41,45 @@ def hide_table_index():
                 """
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
+    # st.dataframe
+    hide_dataframe_row_index = """
+                <style>
+                .row_heading.level0 {display:none}
+                .blank {display:none}
+                </style>
+                """
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+
 
 def get_conversation(d, speaker=True):
     return "\n".join(
         speaker + turn["utterance"]
         for speaker, turn in zip(cycle(["USER:  ", "AGENT: "] if speaker else [""]), d)
     )
+
+
+@st.cache(hash_funcs={BERTScorer: lambda _: None})
+def compute_bert_score(bert_scorer, conversation1, conversation2):
+    bert_score = bert_scorer.score([conversation1], [conversation2])
+    P, R, F1 = [x.item() for x in bert_score]
+    return {"P": P, "R": R, "F1": F1}
+
+
+@st.cache(hash_funcs={RougeScorer: lambda _: None})
+def compute_rouge_scores(rouge_scorer, conversation1, conversation2):
+    rouge_scores = rouge_scorer.score(conversation1, conversation2)
+    new_rouge_scores = {}
+
+    for rouge_type, scores in rouge_scores.items():
+        rouge_type = rouge_type.upper()
+
+        new_rouge_scores[rouge_type] = {
+            f"P": scores.precision,
+            f"R": scores.recall,
+            f"F1": scores.fmeasure,
+        }
+
+    return new_rouge_scores
 
 
 def get_sequence(dialogue, annotations):
@@ -73,25 +121,29 @@ def get_sequence(dialogue, annotations):
     return sequence
 
 
-def flatten(sequence):
-    # return [x for subsequence in sequence for element in subsequence for x in element]
-    return ["_".join(element) for subsequence in sequence for element in subsequence]
+def flatten(sequence, concatenate=False):
+    if concatenate:
+        return [
+            "".join(x.title().replace(" ", "") for x in element)
+            for subsequence in sequence
+            for element in subsequence
+        ]
+    else:
+        return [
+            x for subsequence in sequence for element in subsequence for x in element
+        ]
 
 
 # Initialize
-
-st.set_page_config(layout="wide")
 hide_table_index()
-
-data, scorer = init()
-
+data, bert_scorer, rouge_scorer = init()
 
 # Choose dialogues and write them
 st.header("Dialogues")
 col1, col2 = st.columns(2)
 with col1:
     d1_id, d1 = st.selectbox(
-        "Choose dialogue X", options=data.items(), format_func=lambda x: x[0]
+        "Choose dialogue", options=sorted(data.items()), format_func=lambda x: x[0]
     )
 
     st.write(f"No. turns: {len(d1)}")
@@ -99,19 +151,29 @@ with col1:
 
 with col2:
     d2_id, d2 = st.selectbox(
-        "Choose dialogue Y", options=data.items(), format_func=lambda x: x[0]
+        "Choose reference", options=sorted(data.items()), format_func=lambda x: x[0]
     )
 
     st.write(f"No. turns: {len(d2)}")
     st.text(get_conversation(d2))
 
+# Score text similarity
 st.subheader("Scores")
-rouge = scorer.score(
-    get_conversation(d1, speaker=False), get_conversation(d2, speaker=False)
+conversation1 = get_conversation(d1, speaker=False)
+conversation2 = get_conversation(d2, speaker=False)
+
+bert_score = compute_bert_score(bert_scorer, conversation1, conversation2)
+st.table({f"BERTScore-{k}": [v] for k, v in bert_score.items()})
+
+rouge_scores = compute_rouge_scores(rouge_scorer, conversation1, conversation2)
+rouge_table = {"x": [x[len("ROUGE") :] for x in rouge_scores.keys()]}
+rouge_table.update(
+    {
+        f"ROUGEx-{k}": [scores[k] for _, scores in rouge_scores.items()]
+        for k in ["P", "R", "F1"]
+    }
 )
-rouge = {k: [v.fmeasure] for k, v in rouge.items()}
-scores = rouge
-st.table(scores)
+st.table(rouge_table)
 
 st.header("Sequences")
 
@@ -135,14 +197,39 @@ with col2:
     st.text("\n".join(str(subseq) for subseq in seq2))
 
 
-# Compute similarity scores
+# Flatten sequences
 st.subheader("Flattened sequences")
-flat_seq1 = flatten(seq1)
-flat_seq2 = flatten(seq2)
+concatenate = st.checkbox("Concatenate annotations")
+flat_seq1 = flatten(seq1, concatenate)
+flat_seq2 = flatten(seq2, concatenate)
 st.text(flat_seq1)
 st.text(flat_seq2)
 
+# Compute similarity scores
 st.subheader("Scores")
-scores = scorer.score(" ".join(flat_seq1), " ".join(flat_seq2))
-scores = {k: [v.fmeasure] for k, v in scores.items()}
-st.table(scores)
+
+# ROUGE
+rouge_scores = compute_rouge_scores(
+    rouge_scorer, " ".join(flat_seq1), " ".join(flat_seq2)
+)
+rouge_table = {"x": [x[len("ROUGE") :] for x in rouge_scores.keys()]}
+rouge_table.update(
+    {
+        f"ROUGEx-{k}": [scores[k] for _, scores in rouge_scores.items()]
+        for k in ["P", "R", "F1"]
+    }
+)
+st.table(rouge_table)
+
+# Ratcliff and Obershelp algorithm
+sm = SequenceMatcher(None, flat_seq1, flat_seq2)
+ro_ratio = sm.ratio()
+
+# Levenshtein distance
+ld = textdistance.levenshtein.normalized_similarity(flat_seq1, flat_seq2)
+
+st.table({"ro_ratio": [ro_ratio], "levenshtein": [ld]})
+
+# Longest common subsequence
+st.subheader("Longest common subsequence")
+st.text(textdistance.lcsstr(flat_seq1, flat_seq2))
